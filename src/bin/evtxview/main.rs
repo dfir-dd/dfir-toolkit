b02b6a80f8a9829a6a718e4f41bc4f7dfacb84ea
Jan Starke <jan.starke@posteo.de>
2024-05-08 09:59:15 +0200

    change init behavior

--- src/bin/evtxview/main.rs
+++ src/bin/evtxview/main.rs
@@ -11,6 +11,6 @@ fn main() -> Result<()> {
     let cli = Cli::parse_cli();
     let mut terminal = tui::init()?;
     let app_result = App::new(cli).run(&mut terminal);
-    tui::restore()?;
+    tui::restore(&mut terminal)?;
     Ok(app_result?)
 }
--- src/bin/evtxview/tui/mod.rs
+++ src/bin/evtxview/tui/mod.rs
@@ -4,9 +4,13 @@ pub use evtx_table::*;
 mod color_scheme;
 pub use color_scheme::*;
 
-use std::io::{self, stdout, Stdout};
+use std::io::{self, Stdout};
 
-use crossterm::{execute, terminal::*};
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::*,
+};
 use ratatui::prelude::*;
 
 /// A type alias for the terminal type used in this application
@@ -14,14 +18,23 @@ pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
 /// Initialize the terminal
 pub fn init() -> io::Result<Tui> {
-    execute!(stdout(), EnterAlternateScreen)?;
     enable_raw_mode()?;
-    Terminal::new(CrosstermBackend::new(stdout()))
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let terminal = Terminal::new(backend)?;
+
+    Ok(terminal)
 }
 
 /// Restore the terminal to its original state
-pub fn restore() -> io::Result<()> {
-    execute!(stdout(), LeaveAlternateScreen)?;
+pub fn restore(terminal: &mut Tui) -> io::Result<()> {
     disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
     Ok(())
 }
